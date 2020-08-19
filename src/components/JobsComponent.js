2c8b03a9957dd81216c746fa822edda2e37e24be
Ahmed Goda <ahmedmosad405@gmail.com>
2020-08-19 13:53:29 +0200

    Styling and adding some animations

--- src/components/JobsComponent.js
+++ src/components/JobsComponent.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Loading } from './LoadingComponent'
 import { baseUrl } from '../shared/baseUrl';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer'
 
 const RenderJob = ({job}) => {
+
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            controls.start('visible');
+        } else {
+            controls.start('hidden');
+        }
+    }, [controls, inView])
+
     return (
-        <div className="box">
+        <motion.div ref={ref} initial={'hidden'} animate={controls}
+        variants={{
+            visible: { opacity: 1 },
+            hidden: { opacity: 0 },
+            transition: { transitionDuration: 2}
+        }}
+        className={`box${job.featured && job.new ? ' box-border' : ''}`}>
             <div className="img">
                 <img src={baseUrl + job.logo} alt={job.company} />
             </div>
             <div className="text">
                 <div className="info">
                     <div className="heading">
-                        <h3>{job.company}</h3> {job.new ? (<span className="new">NEW!</span>): null} {job.featured ? (<span className="featured">FEATURED</span>): null}
+                        <h4>{job.company}</h4>{job.new ? (<span className="new">NEW!</span>): null}{job.featured ? (<span className="featured">FEATURED</span>): null}
                     </div>
                     <div className="position">
                         <h2>{job.position}</h2>
@@ -22,9 +42,9 @@ const RenderJob = ({job}) => {
                 </div>
             </div>
             <div className="filters">
-                <span>{job.role}</span><span>{job.level}</span>{job.languages.map(lang => (<span key={Math.floor(Math.random() * 40) + 20}>{lang}</span>))}{job.tools !== 'undefined' ? job.tools.map(tool => (<span key={Math.floor(Math.random() * 50) + 30}>{tool}</span>)): null}
+                <span>{job.role}</span><span>{job.level}</span>{job.languages.map(lang => (<span key={lang}>{lang}</span>))}{job.tools !== undefined ? job.tools.map(tool => (<span key={tool}>{tool}</span>)): null}
             </div>
-        </div>
+        </motion.div>
     );
 }
 
@@ -54,9 +74,9 @@ const Jobs = props => {
     }
     
     return (
-        <div className="container">
+        <motion.div className="container">
             <List />
-        </div>
+        </motion.div>
     );
 
 }
--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Loading = () => {
     return (
-        <div>
+        <div className="loader">
             <span className="fas fa-spinner fa-pulse fa-3x fa-fw"></span>
             <p>Loading . . .</p>
         </div>

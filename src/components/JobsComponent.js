08440d12fd027974a62cca2499fd655eb6a0810c
Ahmed Goda <ahmedmosad405@gmail.com>
2020-08-21 06:01:43 +0200

    Restructuring the job component to avoid babel bug and changing the title icon

--- src/components/JobsComponent.js
+++ src/components/JobsComponent.js
@@ -4,6 +4,197 @@ import { baseUrl } from '../shared/baseUrl';
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer'
 
+const RenderJob = ({job, state, addRole, addLevel, addLanguage, addTool,  handleClick}) => {
+
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            controls.start('visible');
+        } else {
+            controls.start('hidden');
+        }
+    }, [controls, inView])
+
+    return (
+        <motion.div ref={ref} initial={'hidden'} animate={controls}
+        variants={{
+            visible: { opacity: 1 },
+            hidden: { opacity: 0 },
+            transition: { transitionDuration: 2}
+        }}
+        className={`box${job.featured && job.new ? ' box-border' : ''}`}>
+            <div className="img">
+                <img src={baseUrl + job.logo} alt={job.company} />
+            </div>
+            <div className="text">
+                <div className="info">
+                    <div className="heading">
+                        <h4>{job.company}</h4>{job.new ? (<span className="new">NEW!</span>): null}{job.featured ? (<span className="featured">FEATURED</span>): null}
+                    </div>
+                    <div className="position">
+                        <h2>{job.position}</h2>
+                    </div>
+                    <div className="details">
+                        <p>{job.postedAt} <span className="dot"></span> {job.contract} <span className="dot"></span> {job.location}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="filters">
+                <span onClick={
+                    (e) => {
+                        if (e.target.innerHTML !== state.role) {
+                            handleClick(e);
+                            addRole(e.target.innerHTML);
+                        }
+                    }
+                    }>{job.role}</span><span onClick={
+                        (e) => {
+                            if (e.target.innerHTML !== state.level) {
+                                handleClick(e);
+                                addLevel(e.target.innerHTML);
+                            }
+                        }
+
+                    }>{job.level}</span>{job.languages.map(lang => (<span key={lang} onClick={
+                        (e) => {
+                            if (!state.languages.includes(lang)) {
+                                handleClick(e);
+                                addLanguage(state.languages.concat(lang));
+                            }
+                        }
+
+                    }>{lang}</span>))}{job.tools !== undefined ? job.tools.map(tool => (<span key={tool} onClick={
+                        (e) => {
+                            if (!state.tools.includes(tool)) {
+                                handleClick(e);
+                                addTool(state.tools.concat(tool));
+                            }
+                        }
+                    }>{tool}</span>)): null}
+            </div>
+        </motion.div>
+    );
+}
+
+const RenderFilterBox = ({clearFilters}) => {
+
+    return (
+        <div className="box filter-box">
+            <div className="filters"></div>
+            <div className="clear-btn">
+                <button onClick={clearFilters}>Clear</button>
+            </div>
+        </div>
+    );
+}
+
+const List = ({isLoading, errMess, jobs, state, addRole, addLevel, addLanguage, addTool, handleClick}) => {
+
+    if (isLoading) {
+        return (
+            <Loading />
+        );
+    } else if (errMess) {
+        return(
+            <p>{errMess}</p>
+        );
+    } else {
+        return (
+            <>
+                {/* eslint-disable-next-line */}
+                {jobs.filter(job => {
+
+                    // Check if items existed in state
+
+                    const existedRole = job.role === state.role;
+                    const existedLevel = job.level === state.level;
+                    const existedLanguages = state.languages.every(i => job.languages.includes(i));
+                    const existedTools = state.tools.every(i => job.tools.includes(i));
+
+                    // Check if items are null or undefined
+
+                    const isRoleNull = state.role === null;
+                    const isLevelNull = state.level === null;
+                    const isLanguagesUndefined = state.languages.length === 0;
+                    const isToolsUndefined = state.tools.length === 0;
+
+                    if (isRoleNull && isLevelNull && isLanguagesUndefined && isToolsUndefined) {
+                        return job;
+
+                    } else {
+                        if (isLevelNull && isLanguagesUndefined && isToolsUndefined) {
+                            return existedRole;
+
+                        } else if (isRoleNull && isLanguagesUndefined && isToolsUndefined) {
+                            return existedLevel;
+
+                        } else if (isRoleNull && isLevelNull && isToolsUndefined) {
+
+                            return existedLanguages;
+                            
+
+                        } else if (isRoleNull && isLevelNull && isLanguagesUndefined) {
+                            return existedTools;
+
+                        } else {
+
+                            if (isLanguagesUndefined && isToolsUndefined) {
+                                return existedRole && existedLevel;
+
+                            } else if (isLevelNull && isToolsUndefined) {
+                                return existedRole && existedLanguages;
+                                
+                            } else if (isRoleNull && isToolsUndefined) {
+                                return existedLevel && existedLanguages;
+                                
+                            } else if (isLevelNull && isLanguagesUndefined) {
+                                return existedRole && existedTools;
+
+                            } else if (isRoleNull && isLanguagesUndefined) {
+                                return existedLevel && existedTools;
+                                
+                            } else if (isRoleNull && isLevelNull) {
+                                return existedLanguages && existedTools;
+                                
+                            } else {
+
+                                if (isToolsUndefined) {
+                                    return existedRole && existedLevel && existedLanguages
+
+                                } else if (isLanguagesUndefined) {
+                                    return existedRole && existedLevel && existedTools;
+
+                                } else if (isLevelNull) {
+                                    return existedRole && existedLanguages && existedTools;
+
+                                } else if (isRoleNull) {
+                                    return existedLevel && existedLanguages && existedTools;
+
+                                }  else {
+                                    return existedRole && existedLevel && existedLanguages && existedTools;
+                                }
+                            }
+                        } 
+                    }
+                }).map(job => {
+                    return (
+                        <RenderJob key={job.id} job={job} 
+                        state={state} 
+                        addRole={addRole} 
+                        addLevel={addLevel}
+                        addLanguage={addLanguage}
+                        addTool={addTool}
+                        handleClick={handleClick} />
+                    );
+                })}
+            </>
+        );
+        
+    }
+}
+
 class Jobs extends React.Component {
 
     constructor(props) {
@@ -72,162 +263,20 @@ class Jobs extends React.Component {
         document.querySelector('.container-filter').style.display = 'none'
     }
 
-    RenderJob = ({job}) => {
-
-        const controls = useAnimation();
-        const [ref, inView] = useInView();
-    
-        useEffect(() => {
-            if (inView) {
-                controls.start('visible');
-            } else {
-                controls.start('hidden');
-            }
-        }, [controls, inView])
-    
-        return (
-            <motion.div ref={ref} initial={'hidden'} animate={controls}
-            variants={{
-                visible: { opacity: 1 },
-                hidden: { opacity: 0 },
-                transition: { transitionDuration: 2}
-            }}
-            className={`box${job.featured && job.new ? ' box-border' : ''}`}>
-                <div className="img">
-                    <img src={baseUrl + job.logo} alt={job.company} />
-                </div>
-                <div className="text">
-                    <div className="info">
-                        <div className="heading">
-                            <h4>{job.company}</h4>{job.new ? (<span className="new">NEW!</span>): null}{job.featured ? (<span className="featured">FEATURED</span>): null}
-                        </div>
-                        <div className="position">
-                            <h2>{job.position}</h2>
-                        </div>
-                        <div className="details">
-                            <p>{job.postedAt} <span className="dot"></span> {job.contract} <span className="dot"></span> {job.location}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="filters">
-                    <span onClick={
-                        (e) => {
-                            this.handleClick(e);
-                            this.setState({role: e.target.innerHTML});
-                        }
-                        }>{job.role}</span><span onClick={
-                            (e) => {
-                            this.handleClick(e);
-                            this.setState({level: e.target.innerHTML});
-                            }
-                        }>{job.level}</span>{job.languages.map(lang => (<span key={lang} onClick={
-                            (e) => {
-                                this.handleClick(e);
-                                this.setState({languages: this.state.languages.concat(lang)});
-                            }
-                        }>{lang}</span>))}{job.tools !== undefined ? job.tools.map(tool => (<span key={tool} onClick={
-                            (e) => {
-                                this.handleClick(e);
-                                this.setState({tools: this.state.tools.concat(tool)});
-                            }
-                        }>{tool}</span>)): null}
-                </div>
-            </motion.div>
-        );
-    }
-    
-
-    RenderFilterBox = () => {
-
-        return (
-            <div className="box filter-box">
-                <div className="filters"></div>
-                <div className="clear-btn">
-                    <button onClick={this.clearFilters}>Clear</button>
-                </div>
-            </div>
-        );
+    addRole = (value) => {
+        this.setState({role: value})
     }
 
-    List = () => {
-        if (this.props.isLoading) {
-            return (
-                <Loading />
-            );
-        } else if (this.props.errMess) {
-            return(
-                <p>{this.props.errMess}</p>
-            );
-        } else {
-            return (
-                <>
-                    {/* eslint-disable-next-line */}
-                    {this.props.jobs.filter(job => {
-                        if (this.state.role === null && this.state.level === null && this.state.languages.length === 0 && this.state.tools.length === 0) {
-                            return job;
-                        } else {
-                            if (this.state.level === null && this.state.languages.length === 0 && this.state.tools.length === 0) {
-                                return job.role === this.state.role;
+    addLevel = (value) => {
+        this.setState({level: value})
+    }
 
-                            } else if (this.state.role === null && this.state.languages.length === 0 && this.state.tools.length === 0) {
-                                return job.level === this.state.level;
+    addLanguage = (value) => {
+        this.setState({languages: this.state.languages.concat(value)});
+    }
 
-                            } else if (this.state.role === null && this.state.level === null && this.state.tools.length === 0) {
-
-                                return this.state.languages.every(i => job.languages.includes(i))
-                                
-
-                            } else if (this.state.role === null && this.state.level === null && this.state.languages.length === 0) {
-                                return this.state.tools.every(i => job.tools.includes(i))
-                            } else {
-
-                                if (this.state.languages.length === 0 && this.state.tools.length === 0) {
-                                    return job.role === this.state.role && job.level === this.state.level;
-
-                                } else if (this.state.level === null && this.state.tools.length === 0) {
-                                    return job.role === this.state.role && this.state.languages.every(i => job.languages.includes(i));
-                                    
-                                } else if (this.state.role === null && this.state.tools.length === 0) {
-                                    return job.level === this.state.level && this.state.languages.every(i => job.languages.includes(i));
-                                    
-                                } else if (this.state.level === null && this.state.languages.length === 0) {
-                                    return job.role === this.state.role && this.state.tools.every(i => job.tools.includes(i));
-
-                                } else if (this.state.role === null && this.state.languages.length === 0) {
-                                    return job.level === this.state.level && this.state.tools.every(i => job.tools.includes(i));
-                                    
-                                } else if (this.state.role === null && this.state.level === null) {
-                                    return this.state.languages.every(i => job.languages.includes(i)) && this.state.tools.every(i => job.tools.includes(i));
-                                    
-                                } else {
-
-                                    if (this.state.tools.length === 0) {
-                                        return job.role === this.state.role && job.level === this.state.level && this.state.languages.every(i => job.languages.includes(i))
-
-                                    } else if (this.state.languages.length === 0) {
-                                        return job.role === this.state.role && job.level === this.state.level && this.state.tools.every(i => job.tools.includes(i));
-
-                                    } else if (this.state.level === null) {
-                                        return job.role === this.state.role && this.state.languages.every(i => job.languages.includes(i)) && this.state.tools.every(i => job.tools.includes(i));
-
-                                    } else if (this.state.role === null) {
-                                        return job.level === this.state.level && this.state.languages.every(i => job.languages.includes(i)) && this.state.tools.every(i => job.tools.includes(i));
-
-                                    }  else {
-                                        return job.role === this.state.role && job.level === this.state.level && this.state.languages.every(i => job.languages.includes(i)) && this.state.tools.every(i => job.tools.includes(i));
-                                    }
-                                }
-                            } 
-                        }
-                    }).map(job => {
-                        return (
-                            <this.RenderJob key={job.id} job={job} />
-                        );
-                    })}
-                </>
-            );
-            
-        }
+    addTool = (value) => {
+        this.setState({tools: this.state.tools.concat(value)});
     }
     
     render() {
@@ -235,10 +284,18 @@ class Jobs extends React.Component {
             <>
                 <div className="container container-filter" style={{display: "none"}}>
                     {/* {console.log(this.state)} */}
-                    <this.RenderFilterBox />
+                    <RenderFilterBox clearFilters={this.clearFilters} />
                 </div>
                 <motion.div className="container">
-                    <this.List />
+                    <List isLoading={this.props.isLoading}
+                        errMess={this.props.errMess}
+                        jobs={this.props.jobs}
+                        state={this.state}
+                        addRole={this.addRole}
+                        addLevel={this.addLevel}
+                        addLanguage={this.addLanguage}
+                        addTool={this.addTool}
+                        handleClick={this.handleClick} />
                 </motion.div>
             </>
     );
